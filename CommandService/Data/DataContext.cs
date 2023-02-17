f287aaf13e64e6a1f16b6922a676406865b8e80b
Kprzyby <kprzy@vp.pl>
2023-02-17 18:48:40 +0100

    configured basic EFC

---
+++ CommandService/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CommandService.Data
+{
+    public class DataContext : DbContext
+    {
+        #region Constructors
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        #endregion Constructors
+    }
+}
