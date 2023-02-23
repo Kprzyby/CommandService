e0f897707a5e740ad59b51baed88d219d20a1da7
Kprzyby <kprzy@vp.pl>
2023-02-23 20:55:59 +0100

    implemented paging, filtering and sorting for the platform controller and updated the documentation

--- CommandService/Controllers/PlatformController.cs
+++ CommandService/Controllers/PlatformController.cs
@@ -1,5 +1,6 @@
 ﻿using CommandService.Data.DTOs.Platform;
 using CommandService.Services;
+using CommandService.ViewModels.Platform;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandService.Controllers
@@ -27,23 +28,40 @@ namespace CommandService.Controllers
         /// <summary>
         /// Asynchronous method for loading all platforms
         /// </summary>
-        /// <returns>A list of objects containing information about each platform</returns>
+        /// <param name="filterViewModel">Object containing information about the paging, filtering and order</param>
+        /// <remarks>
+        /// The number of the first page is 1 and the minimal size of the page is also 1.
+        ///
+        /// The "SortInfo" parameter's key should be "Name" and its value - "asc" or "desc" depending on the desired sort order.
+        /// If this parameter is not provided, the platforms will be sorted by name ascendingly.
+        ///
+        /// This method returns all platforms staring with the value of the "NameFilterValue" parameter (not case sensitive).
+        /// </remarks>
+        /// <returns>Object containing a list of platforms along with information about paging, filtering and order</returns>
         /// <response code="500">Error message</response>
-        /// <response code="200">A list of objects containing information about each platform</response>
+        /// <response code="200">Object containing a list of platforms along with information about paging, filtering and order</response>
         [HttpGet]
         [Route("c/Platform/GetPlatformsAsync")]
         [ProducesResponseType(typeof(string), 500)]
-        [ProducesResponseType(typeof(List<ReadPlatformDTO>), 200)]
-        public async Task<IActionResult> GetPlatformsAsync()
+        [ProducesResponseType(typeof(ReadPlatformsResponseDTO), 200)]
+        public async Task<IActionResult> GetPlatformsAsync(ReadPlatformsViewModel filterViewModel)
         {
-            var platforms = await _platformService.GetPlatformsAsync();
+            PlatformFilteringDTO dto = new PlatformFilteringDTO()
+            {
+                PageSize = filterViewModel.PageSize,
+                PageNumber = filterViewModel.PageNumber,
+                SortInfo = filterViewModel.SortInfo,
+                NameFilterValue = filterViewModel.NameFilterValue,
+            };
 
-            if (platforms == null)
+            var response = await _platformService.GetPlatformsAsync(dto);
+
+            if (response == null)
             {
                 return StatusCode(500, "Server error while executing the operation");
             }
 
-            return Ok(platforms);
+            return Ok(response);
         }
 
         #endregion Methods
---
+++ CommandService/Data/DTOs/Platform/PlatformFilteringDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CommandService.Data.DTOs.Platform
+{
+    public class PlatformFilteringDTO
+    {
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+        public KeyValuePair<string, string>? SortInfo { get; set; }
+        public string? NameFilterValue { get; set; }
+    }
+}
